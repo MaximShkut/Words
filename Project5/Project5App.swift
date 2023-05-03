a2008c14be2e5bbdd117bd41d888bcb8add5405b
MaximShkut <m.shkut344@gmail.com>
2023-05-03 21:00:17 +0200

    Initial Commit

---
+++ Project5/Project5App.swift
@@ -0,0 +1,17 @@
+//
+//  Project5App.swift
+//  Project5
+//
+//  Created by user236450 on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Project5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
